186ffbb7193da2cf5bb2f309381d5fc45e1fae5d
tipikae <gilles.bd@free.fr>
2023-03-13 15:38:55 +0100

    Chapter 3

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
 
 
@@ -10,7 +11,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     CommonModule
   ],
   exports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule
   ]
 })
 export class SharedModule { }
