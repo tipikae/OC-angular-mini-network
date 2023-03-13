f1bcaa8c82ac8eb68ea16ed42b994e7ae3752c0b
tipikae <gilles.bd@free.fr>
2023-03-13 17:20:56 +0100

    chapter 4

---
+++ src/app/shared/components/comments/comments.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Comment } from 'src/app/core/models/comment.model';
+
+@Component({
+  selector: 'app-comments',
+  templateUrl: './comments.component.html',
+  styleUrls: ['./comments.component.scss']
+})
+export class CommentsComponent implements OnInit {
+
+  @Input() comments!: Comment[];
+  @Output() newComment = new EventEmitter<string>();
+
+  commentCtrl!: FormControl;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.commentCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(10)]);
+  }
+
+  onLeaveComment() {
+    if (this.commentCtrl.invalid) {
+      return;
+    }
+    this.newComment.emit(this.commentCtrl.value);
+    this.commentCtrl.reset();
+  }
+
+}
--- src/app/social-media/components/post-list/post-list.component.ts
+++ src/app/social-media/components/post-list/post-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { map, Observable } from 'rxjs';
 import { Post } from '../../models/post.model';
+import { PostsService } from '../../services/posts.service';
 
 @Component({
   selector: 'app-post-list',
@@ -12,7 +13,8 @@ export class PostListComponent implements OnInit {
 
   posts$!: Observable<Post[]>;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private postsService: PostsService) { }
 
   ngOnInit(): void {
     this.posts$ = this.route.data.pipe(
@@ -20,4 +22,7 @@ export class PostListComponent implements OnInit {
     );
   }
 
+  onPostCommented(postCommented: { comment: string, postId: number }) {
+    this.postsService.addNewComment(postCommented);
+  }
 }
